18f80a4e784afcb18d3ea2272a2a36b192b2d6cd
Diego Palacio <diegopalacio@politecnicodecolombia.edu.co>
2024-01-26 10:01:28 -0500

    Almancenimiento de videos en aws para campus virtual

--- app/libs/autoload.php
+++ app/libs/autoload.php
@@ -1,13 +1,16 @@
 <?php
 
 require_once('jwt/JWToken.php'); 
+require_once('bucket/Bucket.php'); 
 
 class AutloadLibs {
     
     private $JWToken    = null;
+    private $Bucket     = null;
 
     public function __construct() {
         
         $this->JWToken      = new JWToken();
+        $this->Bucket       = new Bucket();
     }
 }
--- app/libs/env/Env.php
+++ app/libs/env/Env.php
@@ -12,7 +12,7 @@ class Env {
     public function __construct(){
 
         $this->Env = new Dotenv();
-        $this->Env->load('../../../../../../envs/.env-campus');
+        $this->Env->load('../../../../../envs/.env-campus');
 
         $this->VariablesEntorno = $_ENV;
     }
